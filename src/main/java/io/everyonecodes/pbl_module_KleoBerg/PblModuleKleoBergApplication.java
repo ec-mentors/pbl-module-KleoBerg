f4099c3ba9896f6a8a03203ae60d60442f2ab9f9
Kleo Berghuber <kleo.berghuber@gmail.com>
2025-06-19 15:17:43 +0200

    First commit Project setup

---
+++ src/main/java/io/everyonecodes/pbl_module_KleoBerg/PblModuleKleoBergApplication.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.pbl_module_KleoBerg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PblModuleKleoBergApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PblModuleKleoBergApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/everyonecodes/pbl_module_KleoBerg/PblModuleKleoBergApplicationTests.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.pbl_module_KleoBerg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PblModuleKleoBergApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
